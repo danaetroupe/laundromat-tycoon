1b639813aaa7938fb3d001453283a63dd23a9e14
danaetroupe <danaetroupe@gmail.com>
2024-11-06 16:52:36 -0500

    [UNSTABLE] Working on UI story

--- src/StarterPlayerScripts/Build/init.client.lua
+++ src/StarterPlayerScripts/Build/init.client.lua
@@ -4,7 +4,8 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local PlayerService = require(ReplicatedStorage.Services.PlayerService)
 
-local Objects = require(ReplicatedStorage.Data.ObjectData.Objects)
+local StoreScript = require(ReplicatedStorage.UI.Store)
+
 local LocalPlayer = Players.LocalPlayer
 
 local PlayerGui = LocalPlayer.PlayerGui
@@ -13,44 +14,17 @@ local BuildFrame : Frame = PanelsGui.Build
 local BuildPanel = BuildFrame:FindFirstChild("Panel")
 
 local Store : ScrollingFrame = BuildPanel:FindFirstChild("Store")
-local Inventory : ScrollingFrame = BuildPanel:FindFirstChild("Inventory")
+--local Inventory : ScrollingFrame = BuildPanel:FindFirstChild("Inventory")
 
-local PlaceObject = require(script.PlaceObject)
 
 local Icon = ReplicatedStorage.Assets.UI:WaitForChild("Icon")
-local Mouse = LocalPlayer:GetMouse()
 
 local isBuilding = false
 
--- Clear UI page for reset
-local function clear()
-    for _, icon in BuildPanel.Store:GetChildren() do
-        if icon:IsA("ImageButton") then
-            icon:Destroy()
-        end
-    end
-end
 
-local function showStore()
-    clear()
-    for _, objectName in Objects.GetObjectNames() do
-        local button = Icon:Clone()
-        button.Parent = Store
-        --[[
-        todo: implement image
-        local image = objects.GetProperty(objectName, "image")
-        button.Image =  if image then image else button.Image
-        ]]
-        button.Top.Text = Objects.GetProperty(objectName, "name")
-        local cost = Objects.GetProperty(objectName, "cost")
-        button.Display.Text = if cost then "$"..cost else "$ERR"
-        PlaceObject(button, objectName, BuildFrame)
-     end
-end
 
 local function showInventory()
-    clear()
-
+    
 end
 
 BuildFrame.Changed:Connect(function(property)
@@ -59,6 +33,8 @@ BuildFrame.Changed:Connect(function(property)
     PlayerService.SetBuildState(isBuilding)
     if isBuilding then 
         -- Reset to store when opening and closing panel
-        showStore()
+        StoreScript.show(Store, Icon)
+    else
+        StoreScript.hide()
     end
 end)
